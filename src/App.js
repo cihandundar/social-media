6545fae49f6d1ee7432c77e5c47ac1f300e4a3f7
cihandundar <94462152+cihandundar@users.noreply.github.com>
2023-08-04 14:58:37 +0300

    header page imported

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import Header from "components/Header";
 import { Home, Login, Register } from "pages";
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 function App() {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
