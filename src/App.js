db5451fef1db294092719cad5ce48ce2b1e2a28e
cihandundar <94462152+cihandundar@users.noreply.github.com>
2023-08-04 13:56:26 +0300

    page structures edited

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
+import { Home, Login, Register } from "pages";
 import React from "react";
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 function App() {
-  return <div className="App"></div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
