4b436b08782d88db478517187f50cdaf061ec763
cihandundar <94462152+cihandundar@users.noreply.github.com>
2023-08-04 14:59:40 +0300

    register page created

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,7 +1,46 @@
 import React from "react";
-
+import Logo from "../../assets/images/logo.jpg";
 const Login = () => {
-  return <div>Login</div>;
+  return (
+    <section className="register">
+      <div className="register__container">
+        <div className="left">
+          <img src={Logo} alt="" />
+        </div>
+        <div className="right">
+          <div>
+            <h1>Sign In</h1>
+          </div>
+          <div>
+            <form className="form">
+              <div className="form__group">
+                <input
+                  type="email"
+                  className="form__control"
+                  id="email"
+                  name="email"
+                  placeholder="Email"
+                />
+              </div>
+              <div className="form__group">
+                <input
+                  type="password"
+                  className="form__control"
+                  id="password"
+                  name="password"
+                  placeholder="Password"
+                />
+              </div>
+
+              <div className="form__group">
+                <button type="submit">Sign in</button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Login;
