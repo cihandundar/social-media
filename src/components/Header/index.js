9ff2371552f819b4b4074ff184f4611f680040d0
cihandundar <94462152+cihandundar@users.noreply.github.com>
2023-08-04 17:22:57 +0300

    header component created

---
+++ src/components/Header/index.js
@@ -0,0 +1,59 @@
+import React from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { FaSignInAlt, FaUser } from "react-icons/fa";
+import { useDispatch, useSelector } from "react-redux";
+import { logout, reset } from "../../features/auth/authSlice";
+
+const Header = () => {
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  const { user } = useSelector((state) => state.auth);
+
+  const onLogout = () => {
+    dispatch(logout());
+    dispatch(reset());
+    navigate("/login");
+  };
+
+  return (
+    <header className="header">
+      <div className="header__container">
+        <div className="logo">
+          <Link to="/">
+            <h2>22 Media</h2>
+          </Link>
+        </div>
+        <ul>
+          {user ? (
+            <ul>
+              <li>
+                <span>Hi {user.displayName}</span>
+              </li>
+              <li>
+                <button onClick={onLogout} className="btn">
+                  Logout
+                </button>
+              </li>
+            </ul>
+          ) : (
+            <ul>
+              <li>
+                <Link to="/login">
+                  <FaSignInAlt /> Login
+                </Link>
+              </li>
+              <li>
+                <Link to="/register">
+                  <FaUser /> Register
+                </Link>
+              </li>
+            </ul>
+          )}
+        </ul>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
