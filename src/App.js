d93d427c04a7d3f3cf5df0b6a91b788f69e352ef
cihandundar <94462152+cihandundar@users.noreply.github.com>
2023-08-04 17:21:42 +0300

    toastify added

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import Header from "components/Header";
 import { Home, Login, Register } from "pages";
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 function App() {
   return (
     <BrowserRouter>
@@ -11,6 +13,18 @@ function App() {
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
       </Routes>
+      <ToastContainer
+        position="top-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light"
+      />
     </BrowserRouter>
   );
 }
