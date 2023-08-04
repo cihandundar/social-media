0828f308c8cb592393e66757e951fc0c04df8ae9
cihandundar <94462152+cihandundar@users.noreply.github.com>
2023-08-04 17:25:11 +0300

    login page created

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,6 +1,54 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Logo from "../../assets/images/logo.jpg";
+import { useNavigate } from "react-router-dom";
+import { login, reset } from "../../features/auth/authSlice";
+import { useDispatch, useSelector } from "react-redux";
+import { toast } from "react-toastify";
+
 const Login = () => {
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  const { user, isLoading, isError, isSuccess, message } = useSelector(
+    (state) => state.auth
+  );
+
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+
+  const { email, password } = formData;
+
+  const onChange = (e) => {
+    setFormData((prevState) => ({
+      ...prevState,
+      [e.target.name]: e.target.value,
+    }));
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+
+    const userData = {
+      email,
+      password,
+    };
+    dispatch(login(userData));
+  };
+
+  useEffect(() => {
+    if (isError) {
+      toast.error(message);
+    }
+
+    if (isSuccess || user) {
+      navigate("/");
+    }
+
+    dispatch(reset());
+  }, [user, isError, isSuccess, message, navigate, dispatch]);
+
   return (
     <section className="register">
       <div className="register__container">
@@ -12,14 +60,16 @@ const Login = () => {
             <h1>Sign In</h1>
           </div>
           <div>
-            <form className="form">
+            <form onSubmit={onSubmit} className="form">
               <div className="form__group">
                 <input
                   type="email"
                   className="form__control"
                   id="email"
                   name="email"
+                  value={email}
                   placeholder="Email"
+                  onChange={onChange}
                 />
               </div>
               <div className="form__group">
@@ -28,7 +78,9 @@ const Login = () => {
                   className="form__control"
                   id="password"
                   name="password"
+                  value={password}
                   placeholder="Password"
+                  onChange={onChange}
                 />
               </div>
 
