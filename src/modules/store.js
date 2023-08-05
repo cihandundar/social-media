10fc34bceb543766550f6900a1bf7c74d505b84f
cihandundar <94462152+cihandundar@users.noreply.github.com>
2023-08-05 23:18:48 +0300

    Imported to userslice store page

--- src/modules/store.js
+++ src/modules/store.js
@@ -1,9 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "../features/auth/authSlice";
-
+import postReducer from "../features/user/userSlice";
 export const store = configureStore({
   reducer: {
     auth: authReducer,
+    post: postReducer,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
