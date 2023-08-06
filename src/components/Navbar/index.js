1455e0162a9bcd8f7f85ff9c244cb850339cc3a8
cihandundar <94462152+cihandundar@users.noreply.github.com>
2023-08-06 13:33:03 +0300

    navbar page edited

---
+++ src/components/Navbar/index.js
@@ -0,0 +1,40 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { CgProfile } from "react-icons/cg";
+import { MdOutlineExplore, MdGroups2 } from "react-icons/md";
+import { SiOpenbadges } from "react-icons/si";
+import { PiTelevisionSimpleBold } from "react-icons/pi";
+const Navbar = () => {
+  return (
+    <nav className="nav">
+      <div className="nav__container">
+        <h3>New Feeds</h3>
+        <ul className="nav__container__list">
+          <li className="nav__container__list__link">
+            <CgProfile />
+            <Link to="/profile">Profile</Link>
+          </li>
+          <li className="nav__container__list__link">
+            <PiTelevisionSimpleBold />
+            <Link to="/profile">Newsfeed</Link>
+          </li>
+          <li className="nav__container__list__link">
+            <SiOpenbadges />
+            <Link to="/profile">Badges</Link>
+          </li>
+          <li className="nav__container__list__link">
+            <MdOutlineExplore />
+            <Link to="/profile">Explore Stories</Link>
+          </li>
+          <li className="nav__container__list__link">
+            <MdGroups2 />
+            <Link to="/profile">Popular Groups</Link>
+          </li>
+        </ul>
+        <ul className="nav__container__list"></ul>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
