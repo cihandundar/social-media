3ca2784ff18e0aeef19561543b813717ea984e64
cihandundar <94462152+cihandundar@users.noreply.github.com>
2023-08-10 23:20:39 +0300

    fix

--- src/pages/Register/index.js
+++ src/pages/Register/index.js
@@ -8,7 +8,7 @@ const Register = () => {
   const navigate = useNavigate();
   const dispatch = useDispatch();
 
-  const { user, isLoading, isError, isSuccess, message } = useSelector(
+  const { user, isError, isSuccess, message } = useSelector(
     (state) => state.auth
   );
 
