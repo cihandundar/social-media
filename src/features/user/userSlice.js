c07f544e14fd46cd346ae69f5724852290c75242
cihandundar <94462152+cihandundar@users.noreply.github.com>
2023-08-10 23:19:07 +0300

    detail page slice added

--- src/features/user/userSlice.js
+++ src/features/user/userSlice.js
@@ -15,6 +15,16 @@ export const fetchUser = createAsyncThunk("user/fetchUser", async () => {
   return response.data;
 });
 
+export const fetchUserDetails = createAsyncThunk(
+  "users/fetchUserDetails",
+  async (id) => {
+    const response = await axios.get(
+      `https://64542599c18adbbdfeb058b1.mockapi.io/posts/${id}`
+    );
+    return response.data;
+  }
+);
+
 export const userSlice = createSlice({
   name: "post",
   initialState,
@@ -30,6 +40,20 @@ export const userSlice = createSlice({
     builder.addCase(fetchUser.rejected, (state, action) => {
       state.error = action.error.message;
       state.isLoading = false;
+    });
+    builder.addCase(fetchUserDetails.pending, (state) => {
+      state.isLoading = true;
+      state.status = "loading";
+    });
+    builder.addCase(fetchUserDetails.fulfilled, (state, action) => {
+      state.details = action.payload;
+      state.isLoading = false;
+      state.status = "success";
+    });
+    builder.addCase(fetchUserDetails.rejected, (state, action) => {
+      state.error = action.error.message;
+      state.isLoading = false;
+      state.status = "failed";
     });
   },
 });
