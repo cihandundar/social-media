f1d79e711c27d953ec41ed834856d5e122ff7406
cihandundar <94462152+cihandundar@users.noreply.github.com>
2023-08-13 12:49:17 +0300

    fix

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -5,52 +5,56 @@ import { useSelector } from "react-redux";
 
 const Profile = () => {
   const { user } = useSelector((state) => state.auth);
-
+  const isLoading = useSelector((state) => state?.post?.isLoading);
   return (
     <section className="profile">
-      <div className="profile__container">
-        <div className="profile__background">
-          <img src={Background} alt="" />
-        </div>
-        <div className="profile__background__info">
-          <div className="profile__background__info__wrapper">
-            <img src={Recep} alt="" />
-            <div>
-              <div className="profile__background__info__title">
-                <span style={{ color: "white" }}> {user.displayName}</span>
-              </div>
-              <div className="profile__background__info__title">
-                <p>Güngören, İstanbul</p>
+      {isLoading ? (
+        <p className="loading">Loading...</p>
+      ) : (
+        <div className="profile__container">
+          <div className="profile__background">
+            <img src={Background} alt="" />
+          </div>
+          <div className="profile__background__info">
+            <div className="profile__background__info__wrapper">
+              <img src={Recep} alt="" />
+              <div>
+                <div className="profile__background__info__title">
+                  <span style={{ color: "white" }}> {user.displayName}</span>
+                </div>
+                <div className="profile__background__info__title">
+                  <p>Güngören, İstanbul</p>
+                </div>
               </div>
             </div>
-          </div>
-          <div className="profile__background__info__right">
-            <button>Update Profile</button>
-            <button>Activity Log</button>
-          </div>
-        </div>
-        <div className="profile__content">
-          <div className="profile__content__wrapper">
-            <div className="profile__content__left">
-              <div className="profile__content__left__title">
-                <h3>Contact</h3>
-              </div>
-              <div className="profile__content__left__info">
-                <p>recep_*!?-ivedik95"@hotmail.com</p>
-              </div>
-              <div className="profile__content__left__info">
-                <h3>About Us</h3>
-                <p>
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta
-                  delectus ratione natus maiores sunt ipsam iure, quasi
-                  assumenda harum omnis.
-                </p>
-              </div>
+            <div className="profile__background__info__right">
+              <button>Update Profile</button>
+              <button>Activity Log</button>
             </div>
-            <div className="profile__content__right"></div>
+          </div>
+          <div className="profile__content">
+            <div className="profile__content__wrapper">
+              <div className="profile__content__left">
+                <div className="profile__content__left__title">
+                  <h3>Contact</h3>
+                </div>
+                <div className="profile__content__left__info">
+                  <p>recep_*!?-ivedik95"@hotmail.com</p>
+                </div>
+                <div className="profile__content__left__info">
+                  <h3>About Us</h3>
+                  <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Dicta delectus ratione natus maiores sunt ipsam iure, quasi
+                    assumenda harum omnis.
+                  </p>
+                </div>
+              </div>
+              <div className="profile__content__right"></div>
+            </div>
           </div>
         </div>
-      </div>
+      )}
     </section>
   );
 };
--- src/pages/ProfileDetails/index.js
+++ src/pages/ProfileDetails/index.js
@@ -5,6 +5,7 @@ import { useParams } from "react-router-dom";
 
 const ProfileDetails = () => {
   const details = useSelector((state) => state?.post?.details);
+  const isLoading = useSelector((state) => state?.post?.isLoading);
   const dispatch = useDispatch();
   const { id } = useParams();
 
@@ -13,45 +14,49 @@ const ProfileDetails = () => {
   }, [dispatch, id]);
   return (
     <section className="profile">
-      <div className="profile__container">
-        <div className="profile__background">
-          <img src={details.images} alt="" />
-        </div>
-        <div className="profile__background__info">
-          <div className="profile__background__info__wrapper">
-            <img src={details.avatar} alt="" />
-            <div>
-              <div className="profile__background__info__title">
-                <span style={{ color: "white" }}> {details.title}</span>
-              </div>
-              <div className="profile__background__info__title">
-                <p>{details.location}</p>
+      {isLoading ? (
+        <p className="loading">Loading...</p>
+      ) : (
+        <div className="profile__container">
+          <div className="profile__background">
+            <img src={details.images} alt="" />
+          </div>
+          <div className="profile__background__info">
+            <div className="profile__background__info__wrapper">
+              <img src={details.avatar} alt="" />
+              <div>
+                <div className="profile__background__info__title">
+                  <span style={{ color: "white" }}> {details.title}</span>
+                </div>
+                <div className="profile__background__info__title">
+                  <p>{details.location}</p>
+                </div>
               </div>
             </div>
-          </div>
-          <div className="profile__background__info__right">
-            <button>Add User</button>
-            <button>Message</button>
-          </div>
-        </div>
-        <div className="profile__content">
-          <div className="profile__content__wrapper">
-            <div className="profile__content__left">
-              <div className="profile__content__left__title">
-                <h3>Contact</h3>
-              </div>
-              <div className="profile__content__left__info">
-                <p>{details.email}</p>
-              </div>
-              <div className="profile__content__left__info">
-                <h3>About Us</h3>
-                <p>{details.description}</p>
-              </div>
+            <div className="profile__background__info__right">
+              <button>Add User</button>
+              <button>Message</button>
             </div>
-            <div className="profile__content__right"></div>
+          </div>
+          <div className="profile__content">
+            <div className="profile__content__wrapper">
+              <div className="profile__content__left">
+                <div className="profile__content__left__title">
+                  <h3>Contact</h3>
+                </div>
+                <div className="profile__content__left__info">
+                  <p>{details.email}</p>
+                </div>
+                <div className="profile__content__left__info">
+                  <h3>About Us</h3>
+                  <p>{details.description}</p>
+                </div>
+              </div>
+              <div className="profile__content__right"></div>
+            </div>
           </div>
         </div>
-      </div>
+      )}
     </section>
   );
 };
