e3e97a17401dc392c9d2ca9c3716d4862f84ff77
cihandundar <94462152+cihandundar@users.noreply.github.com>
2023-08-22 23:58:01 +0300

    fix

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Recep from "../../assets/images/recep.jpg";
 import Background from "../../assets/images/background.jpg";
 import { useSelector } from "react-redux";
@@ -6,6 +6,52 @@ import { useSelector } from "react-redux";
 const Profile = () => {
   const { user } = useSelector((state) => state.auth);
   const isLoading = useSelector((state) => state?.post?.isLoading);
+
+  const [todoList, setTodoList] = useState([]);
+  const [newTodo, setNewTodo] = useState("");
+  const [editedTodo, setEditedTodo] = useState({ index: -1, text: "" });
+
+  useEffect(() => {
+    const storedTodos = localStorage.getItem("todoList");
+    if (storedTodos) {
+      setTodoList(JSON.parse(storedTodos));
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem("todoList", JSON.stringify(todoList));
+  }, [todoList]);
+
+  const handleTodoChange = (event) => {
+    setNewTodo(event.target.value);
+  };
+
+  const handleAddTodo = () => {
+    if (newTodo.trim() !== "") {
+      setTodoList([...todoList, { text: newTodo, isCompleted: false }]);
+      setNewTodo("");
+    }
+  };
+
+  const handleEditTodo = (index) => {
+    const todoToEdit = todoList[index];
+    setEditedTodo({ index, text: todoToEdit.text });
+  };
+
+  const handleSaveEdit = () => {
+    if (editedTodo.text.trim() !== "") {
+      const updatedTodos = [...todoList];
+      updatedTodos[editedTodo.index].text = editedTodo.text;
+      setTodoList(updatedTodos);
+      setEditedTodo({ index: -1, text: "" });
+    }
+  };
+
+  const handleRemoveTodo = (index) => {
+    const updatedTodoList = todoList.filter((_, i) => i !== index);
+    setTodoList(updatedTodoList);
+  };
+
   return (
     <section className="profile">
       {isLoading ? (
@@ -20,7 +66,7 @@ const Profile = () => {
               <img src={Recep} alt="" />
               <div>
                 <div className="profile__background__info__title">
-                  <span style={{ color: "white" }}> {user.displayName}</span>
+                  <span style={{ color: "white" }}>{user.displayName}</span>
                 </div>
                 <div className="profile__background__info__title">
                   <p>Güngören, İstanbul</p>
@@ -50,7 +96,92 @@ const Profile = () => {
                   </p>
                 </div>
               </div>
-              <div className="profile__content__right"></div>
+              <div className="profile__content__right">
+                <div className="profile__content__right__todo">
+                  <div className="profile__content__right__todo__input">
+                    <input
+                      type="text"
+                      placeholder="What's on your mind?"
+                      value={newTodo}
+                      onChange={handleTodoChange}
+                    />
+                    <button onClick={handleAddTodo}>Created Post</button>
+                  </div>
+                  <ul className="profile__content__right__todo__list">
+                    {todoList.map((todo, index) => (
+                      <li key={index}>
+                        <div className="profile__content__right__todo__list__profile">
+                          <img
+                            style={{ width: "50px", borderRadius: "50%" }}
+                            src={Recep}
+                            alt=""
+                          />
+                          <span style={{ color: "white" }}>
+                            {user.displayName}
+                          </span>
+                        </div>
+                        {editedTodo.index === index ? (
+                          <input
+                            type="text"
+                            value={editedTodo.text}
+                            onChange={(event) =>
+                              setEditedTodo({
+                                ...editedTodo,
+                                text: event.target.value,
+                              })
+                            }
+                          />
+                        ) : (
+                          <>
+                            <span
+                              style={{
+                                background: "white",
+                                width: "100%",
+                                height: "37px",
+                                border: "none",
+                                color: "black",
+                                paddingLeft: "10px",
+                                paddingTop: "10px",
+                                overflow: "scroll",
+                                overflowY: "hidden",
+                                overflowX: "hidden",
+                              }}
+                              className={
+                                todo.isCompleted ? "completed" : undefined
+                              }
+                            >
+                              <p> {todo.text}</p>
+                            </span>
+                            <button
+                              className="btn"
+                              style={{ color: "green" }}
+                              onClick={() => handleEditTodo(index)}
+                            >
+                              Edit
+                            </button>
+                          </>
+                        )}
+                        {editedTodo.index === index && (
+                          <button
+                            style={{ color: "green" }}
+                            className="btn"
+                            onClick={handleSaveEdit}
+                          >
+                            Save
+                          </button>
+                        )}
+                        <button
+                          className="btn"
+                          style={{ color: "red" }}
+                          onClick={() => handleRemoveTodo(index)}
+                        >
+                          Remove
+                        </button>
+                      </li>
+                    ))}
+                  </ul>
+                </div>
+              </div>
             </div>
           </div>
         </div>
